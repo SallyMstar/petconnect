d5623e69e467efe90feeb767ce176b31db6b8506
Sally Morningstar <37481835+SallyMstar@users.noreply.github.com>
2021-10-08 12:15:20 -0400

    Update GetPets.js

--- src/GetPets.js
+++ src/GetPets.js
@@ -6,7 +6,7 @@ const GetPets = () => {
         <div className="application">
       <Helmet>
         <script src="http://code.jquery.com/jquery-latest.js" type='text/javascript'>
-         	  $.getJSON("http://api.petfinder.com/pet.find?key=1edf8545fafb2f223f05f30911af67fa&location=45150&output=basic&format=json&callback=?")
+         	  $.getJSON("http://api.petfinder.com/pet.find?key=qYDR44KE32EF0hKonCqN0pBFGiyQghnVVlCwvyMfhhNLd6sb5H&location=45150&output=basic&format=json&callback=?")
         </script>
       </Helmet>
         </div>
